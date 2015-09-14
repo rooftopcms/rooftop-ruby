236456b4a1b44bdd3bc6d88ac4c1463ee83601ca
Ed Jones <ed@errorstudio.co.uk>
2015-09-14 13:07:06 +0100

    Resolve links to parent page

--- lib/rooftop_client/page.rb
+++ lib/rooftop_client/page.rb
@@ -4,10 +4,21 @@ module RooftopClient
       base.include RooftopClient::Base
       base.extend ClassMethods
       base.send(:collection_path,"pages")
+      base.send(:coerce_field, parent: ->(p) { base.send(:resolve_parent_id,p) })
     end
 
     module ClassMethods
-
+      def resolve_parent_id(id)
+        if id == 0
+          #no parent
+          nil
+        else
+          self.send(:find, id)
+        end
+      end
     end
+
+
+
   end
 end
