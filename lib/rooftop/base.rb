6037e9f1560d268f53aaa18c93611e07d916d864
Ed Jones <ed@errorstudio.co.uk>
2015-10-24 21:35:53 +0100

    Add Rooftop::FieldAliases to allow us to alias a field, like this:
    ```
    class Foo
    	include Rooftop::Base
    	alias_field foo: :bar #bar is now an instance method on your model
    end
    ```

--- lib/rooftop/base.rb
+++ lib/rooftop/base.rb
@@ -4,19 +4,28 @@ module Rooftop
       base.include Her::Model
       # Coercions allow you to pass a block to do something with a returned field
       base.include Rooftop::Coercions
+      # Aliases allow you to specify a field (or fields) to alias
+      base.include Rooftop::FieldAliases
       # Queries mixin includes a fixup for there `where()` method
       base.include Rooftop::Queries
       # Links mixin handles the _links key in a response
       base.include Rooftop::ResourceLinks
       # Use the API instance we have configured - in a proc because we can't control load order
       base.send(:use_api,->{Rooftop.configuration.connection})
-      # WP returns an uppercase attribute for ID. Annoying.
-      # base.send(:primary_key, :"ID")
+
       # Date and Modified fields are pretty universal in responses from WP, so we can automatically
       # coerce these to DateTime.
       base.send(:coerce_field,date: ->(date) {DateTime.parse(date)})
       base.send(:coerce_field,modified: ->(modified) {DateTime.parse(modified)})
+
+      # Having coerced the fields, we can alias them (order is important - coerce first.)
+      base.send(:alias_field, date: :created_at)
+      base.send(:alias_field, modified: :updated_at)
+
       base.extend ClassMethods
+
+      # Set up the hooks identified in other mixins. This method is defined in Rooftop::HookCalls
+      base.send(:"setup_hooks!")
     end
 
     module ClassMethods
@@ -24,16 +33,6 @@ module Rooftop
       def first
         all.first
       end
-    end
-
-    # Utility method to make the 'created' field have a more rubyish name
-    def created_at
-      date
-    end
-
-    # Utility method to make the 'updated' field have a more rubyish name
-    def updated_at
-      modified
     end
 
   end
---
+++ lib/rooftop/field_aliases.rb
@@ -0,0 +1,41 @@
+# This module allows you to alias one field as another. There's a bit of a circuitous route to
+# getting it done, because you need to push the after_find hook call onto the end of the hash of
+# existing hook calls. See Rooftop::HookCalls for more details.
+
+module Rooftop
+  module FieldAliases
+    def self.included(base)
+      # Include Rooftop::HookCalls to allow us to push things into a list of hooks in the right order
+      base.include Rooftop::HookCalls
+      base.extend ClassMethods
+
+      # Add the call to the :after_find hook to the list of hook calls, to be processed later.
+      # This is where we iterate over our previously established list of field aliases.
+      base.send(:add_to_hook, :after_find, ->(r){
+        r.field_aliases.each do |old, new|
+          if r.respond_to?(old)
+            r.send("#{new}=",r.send(old))
+          end
+        end
+      })
+
+    end
+
+    module ClassMethods
+      # Call alias_field(foo: :bar) in a class to alias the foo as bar.
+      # @param aliases [Sym] a hash of old and new field names
+      def alias_field(*aliases)
+        @field_aliases ||= {}
+        aliases.each do |alias_hash|
+          @field_aliases.merge!(alias_hash)
+        end
+        @field_aliases
+      end
+    end
+
+    # Class method to get the class's field aliases
+    def field_aliases
+      self.class.instance_variable_get(:"@field_aliases") || {}
+    end
+  end
+end
