294de876fea07c656a763c2ccec7fa338a289410
Ed Jones <ed@errorstudio.co.uk>
2016-01-12 13:41:40 +0000

    Prepend Rooftop::Queries to manage load / inheritance order

--- lib/rooftop/queries/queries.rb
+++ lib/rooftop/queries/queries.rb
@@ -1,50 +1,64 @@
 module Rooftop
   module Queries
     PER_PAGE = 99999999
-    def self.included(base)
-      base.extend ClassMethods
+    def self.prepended(base)
+
+      class << base
+        prepend ClassMethods
+      end
     end
 
     module ClassMethods
+
+
       # We need to fix up the `where()` filter. WP-API expects a url format for filters like this:
-      # /?filter[something]=foo.
+      # /?filter[something]=foo
       def where(args)
-        args = HashWithIndifferentAccess.new(args)
-        # the fact that 'slug' is referred to in the db as 'name' is irritating. Let's fix that
-        # in queries so we can specify {slug: "foo"}
-        if args.keys.collect(&:to_sym).include?(:slug)
-          if args[:slug].is_a?(Array)
-            args[:post_name__in] ||= []
-            args[:slug].each do |slug|
-              args[:post_name__in] << slug
+        filter_format = (args[:filter_format].present? && args[:filter_format]==true) ? true : false
+        args = args.except(:filter_format)
+
+        if filter_format
+          args = HashWithIndifferentAccess.new(args)
+          # the fact that 'slug' is referred to in the db as 'name' is irritating. Let's fix that
+          # in queries so we can specify {slug: "foo"}
+          if args.keys.collect(&:to_sym).include?(:slug)
+            if args[:slug].is_a?(Array)
+              args[:post_name__in] ||= []
+              args[:slug].each do |slug|
+                args[:post_name__in] << slug
+              end
+            else
+              args[:name] = args[:slug]
             end
-          else
-            args[:name] = args[:slug]
+            args.delete(:slug)
           end
-          args.delete(:slug)
-        end
 
-        if args.keys.collect(&:to_sym).include?(:id)
-          if args[:id].is_a?(Array)
-            args[:post__in] ||= []
-            args[:id].each do |id|
-              args[:post__in] << id
+          if args.keys.collect(&:to_sym).include?(:id)
+            if args[:id].is_a?(Array)
+              args[:post__in] ||= []
+              args[:id].each do |id|
+                args[:post__in] << id
+              end
+            else
+              args[:page_id] = args[:id]
             end
-          else
-            args[:page_id] = args[:id]
+            args.delete(:id)
           end
-          args.delete(:id)
+          filters =  args.inject({}) {|hash,pair| hash["filter[#{pair.first}]"] = pair.last; hash}
+
+          # we probably want every result without pagination, unless we specify otherwise
+          filters = {per_page: Rooftop::Queries::PER_PAGE}.merge(filters)
+
+          #Call the Her `where` method with our new filters
+          super(filters)
+        else
+          super
         end
-        filters =  args.inject({}) {|hash,pair| hash["filter[#{pair.first}]"] = pair.last; hash}
-
-        # we probably want every result without pagination, unless we specify otherwise
-        filters = {per_page: Rooftop::Queries::PER_PAGE}.merge(filters)
-
-        #Call the Her `where` method with our new filters
-        super().where(filters)
       end
 
       alias_method :find_by, :where
+
+
 
       def find_by!(args)
         results = find_by(args)
