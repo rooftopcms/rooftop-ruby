b95b60b597fc65b00b3d38bfd0802598e7902f8e
Ed Jones <ed@errorstudio.co.uk>
2016-01-18 23:26:58 +0000

    Call a method to convert the title to a string; DRY

--- lib/rooftop/coercions/title_coercion.rb
+++ lib/rooftop/coercions/title_coercion.rb
@@ -2,12 +2,8 @@ module Rooftop
   module Coercions
     module TitleCoercion
       def self.included(base)
-        base.send(:after_find, ->(record) {
-          if record.respond_to?(:title) && record.title.is_a?(ActiveSupport::HashWithIndifferentAccess)
-            record.title_object = record.title
-            record.title = record.title[:rendered]
-          end
-        })
+        base.send(:after_find, :coerce_title_to_string)
+        base.send(:after_save, :coerce_title_to_string)
 
         base.send(:before_save, ->(record) {
           if record.respond_to?(:title)
@@ -15,12 +11,14 @@ module Rooftop
           end
         })
 
-        base.send(:after_save, ->(record) {
-          if record.respond_to?(:title) && record.title.is_a?(ActiveSupport::HashWithIndifferentAccess)
-            record.title_object = record.title
-            record.title = record.title[:rendered]
-          end
-        })
+      end
+
+      def coerce_title_to_string
+        record = self
+        if record.respond_to?(:title) && record.title.is_a?(ActiveSupport::HashWithIndifferentAccess)
+          record.title_object = record.title
+          record.title = record.title[:rendered]
+        end
       end
     end
   end
