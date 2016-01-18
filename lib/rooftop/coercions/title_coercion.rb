de9f3467aa2cf597c23f9ad09b78987c50dc8afe
Paul Hendrick <paul@errorstudio.co.uk>
2016-01-18 16:35:40 +0000

    check the response object has a title attribute before coercing

--- lib/rooftop/coercions/title_coercion.rb
+++ lib/rooftop/coercions/title_coercion.rb
@@ -3,21 +3,23 @@ module Rooftop
     module TitleCoercion
       def self.included(base)
         base.send(:after_find, ->(record) {
-            if record.title.is_a?(ActiveSupport::HashWithIndifferentAccess)
-              record.title_object = record.title
-              record.title = record.title[:rendered]
-            end
+          if record.respond_to?(:title) && record.title.is_a?(ActiveSupport::HashWithIndifferentAccess)
+            record.title_object = record.title
+            record.title = record.title[:rendered]
+          end
         })
 
         base.send(:before_save, ->(record) {
+          if record.respond_to?(:title)
             record.title_object[:rendered] = record.title
+          end
         })
 
         base.send(:after_save, ->(record) {
-            if record.title.is_a?(ActiveSupport::HashWithIndifferentAccess)
-              record.title_object = record.title
-              record.title = record.title[:rendered]
-            end
+          if record.respond_to?(:title) && record.title.is_a?(ActiveSupport::HashWithIndifferentAccess)
+            record.title_object = record.title
+            record.title = record.title[:rendered]
+          end
         })
       end
     end
