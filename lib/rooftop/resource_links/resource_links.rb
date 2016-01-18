3a48f8693b33b5321bd49019bfd109b6f596cd0c
Ed Jones <ed@errorstudio.co.uk>
2016-01-18 23:27:25 +0000

    Resource links were being erroneously removed before save, which meant that they couldn't be parsed

--- lib/rooftop/resource_links/resource_links.rb
+++ lib/rooftop/resource_links/resource_links.rb
@@ -4,18 +4,24 @@ module Rooftop
     def self.included(base)
       # set up an attribute called resource_links, which is a collection of links
       # to other resources in the API.
-      base.send(:after_find, ->(r) {
-        if r.respond_to?(:"_links")
-          r.resource_links = Rooftop::ResourceLinks::Collection.new(r._links)
-        end
-      })
+      # base.send(:after_find, ->(r) {
+      #   if r.respond_to?(:"_links")
+      #     r.resource_links = Rooftop::ResourceLinks::Collection.new(r._links)
+      #     byebug
+      #   end
+      # })
 
-      base.send(:before_save, ->(r) {
-        r.restore_resource_links! unless r.new?
-      })
+      base.send(:after_find, :generate_resource_links)
+      base.send(:after_save, :generate_resource_links)
 
       base.extend ClassMethods
 
+    end
+
+    def generate_resource_links
+      if self.respond_to?(:"_links")
+        self.resource_links = Rooftop::ResourceLinks::Collection.new(self._links)
+      end
     end
 
     module ClassMethods
