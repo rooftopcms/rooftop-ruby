5089785c63daa7dac25250e6c3de40b7b9fdf19f
Ed Jones <ed@errorstudio.co.uk>
2016-02-28 23:27:35 +0000

    Fix small bug in resolve() where we are looking for an href instance var which doesn't exist

--- lib/rooftop/resource_links/link.rb
+++ lib/rooftop/resource_links/link.rb
@@ -17,7 +17,7 @@ module Rooftop
             return @mapped_class.send(:find, id)
           else
             # otherwise we're going to have make a call to the link's href.
-            @mapped_class.get(@href)
+            @mapped_class.get(href)
           end
         else
           raise Rooftop::ResourceLinks::UnresolvableLinkError, "Couldn't resolve a link of type #{@link_type}. Try passing the class you want to resolve to."
