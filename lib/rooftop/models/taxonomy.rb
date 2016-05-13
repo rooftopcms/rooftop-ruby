6a1b6b9068adf71775d071e988762ef49012e34b
Paul Hendrick <paul@errorstudio.co.uk>
2016-05-13 13:11:11 +0100

    specify api version for the taxonomies endpoint

--- lib/rooftop/models/taxonomy.rb
+++ lib/rooftop/models/taxonomy.rb
@@ -1,6 +1,7 @@
 module Rooftop
   class Taxonomy
     include Rooftop::Base
+    self.api_version = 2
     collection_path "taxonomies"
     primary_key "slug"
     has_many :terms, class_name: "Rooftop::TaxonomyTerm"
