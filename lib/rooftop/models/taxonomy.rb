aacd2e958d8a19abde8402ff2b9eb0388ed4f527
Ed Jones <ed@errorstudio.co.uk>
2016-07-13 16:43:19 +0100

    Working Rooftop:Taxonomy. Note that calling all() will never cache because it's defined higher up the class heirarchy than the Rooftop::Rails::Caching mixin

--- lib/rooftop/models/taxonomy.rb
+++ lib/rooftop/models/taxonomy.rb
@@ -2,8 +2,26 @@ module Rooftop
   class Taxonomy
     include Rooftop::Base
     self.api_version = 2
-    collection_path "taxonomies"
+    self.api_namespace = "wp"
+    self.api_endpoint = "taxonomies"
+
     primary_key "slug"
-    has_many :terms, class_name: "Rooftop::TaxonomyTerm"
+
+    def self.all
+      taxonomies = []
+      get_raw(self.collection_path) do |parsed, raw|
+        parsed[:data].each do |tax,data|
+          taxonomies << Taxonomy.new(data)
+        end
+      end
+      taxonomies.each do |t|
+        t.run_callbacks(:find)
+      end
+      return taxonomies
+    end
+
+    def terms
+      TaxonomyTerm.get(resource_links.find_by(link_type: :item).first.href)
+    end
   end
 end
