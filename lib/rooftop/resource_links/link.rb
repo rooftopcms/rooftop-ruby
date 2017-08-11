70dbb8440f9d6bf30d829f113ee6323b0cde2734
Paul Hendrick <paul@errorstudio.co.uk>
2017-08-11 16:27:40 +0100

    implement marshal_dump and marshal_load to properly store Resource Links in the cache, rather than the OpenStruct#table

--- lib/rooftop/resource_links/link.rb
+++ lib/rooftop/resource_links/link.rb
@@ -2,10 +2,23 @@ module Rooftop
   module ResourceLinks
     class Link < ::OpenStruct
       attr_accessor :link_type
+
       def initialize(link_type,args, klass=nil)
         @link_type = link_type
         @mapped_class = klass.try(:resource_link_mapping).try(:[],@link_type)
+        @klass = klass
+
         super(args)
+      end
+
+      def marshal_dump
+        [@link_type, @table, @klass]
+      end
+
+      def marshal_load(array)
+        @link_type = array[0]
+        @table = array[1]
+        @klass = array[2]
       end
 
       def resolve(klass=nil)
@@ -16,6 +29,10 @@ module Rooftop
           if respond_to?(:id)
             return @mapped_class.send(:find, id)
           else
+            # TODO this is a fudge to get around some hrefs which don't have /wp/v2 in them
+            unless href =~ /wp\/v2/
+              href.gsub!('wp-json','wp-json/wp/v2')
+            end
             # otherwise we're going to have make a call to the link's href.
             result = @mapped_class.get(href)
             result.run_callbacks(:find)
