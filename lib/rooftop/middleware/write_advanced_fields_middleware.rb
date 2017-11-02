ada7dbed5c5d0ec4ed0aec61c524ad911597d985
Ed Jones <ed@errorstudio.co.uk>
2017-11-02 15:57:11 +0000

    remove debug

--- lib/rooftop/middleware/write_advanced_fields_middleware.rb
+++ lib/rooftop/middleware/write_advanced_fields_middleware.rb
@@ -4,7 +4,6 @@ module Rooftop
   class WriteAdvancedFieldsMiddleware < Faraday::Middleware
 
     def call(env)
-      byebug
       # Some post types may allow writing to advanced fields. If this is the case, we need to set a header to tell Rooftop it's OK to write to them.
       if env.body && env.body.has_key?(:write_advanced_fields) && env.body[:write_advanced_fields]
         env[:request_headers]['acf-write-enabled'] = 'true'
