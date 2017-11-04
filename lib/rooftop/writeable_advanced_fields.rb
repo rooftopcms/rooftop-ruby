81b8a53035dea083a21144086f93fec6d959769a
Ed Jones <ed@errorstudio.co.uk>
2017-11-04 12:41:25 +0000

    Call the OPTIONS endpoint to get the advanced fields schema for a class when that class is loaded

--- lib/rooftop/writeable_advanced_fields.rb
+++ lib/rooftop/writeable_advanced_fields.rb
@@ -8,12 +8,16 @@ module Rooftop
 
       base.extend ClassMethods
 
-
     end
 
     module ClassMethods
-      attr_accessor :write_advanced_fields
-      attr_reader :advanced_fields_schema
+      attr_reader :advanced_fields_schema, :write_advanced_fields
+
+      def write_advanced_fields=(truefalse)
+        @write_advanced_fields = truefalse
+        # load the schema now
+        advanced_fields_schema
+      end
       
       def advanced_fields_schema
         @advanced_fields_schema ||= options_raw(collection_path)[:parsed_data][:data][:schema][:properties][:advanced_fields_schema]
@@ -27,6 +31,10 @@ module Rooftop
 
     def advanced_fields_schema
       self.class.advanced_fields_schema
+    end
+
+    def advanced_fields
+      self.class.advanced_fields
     end
 
   end
