4a5ba7ac66b8e2665eedd0e45b7b36c0fddc6937
Ed Jones <ed@errorstudio.co.uk>
2017-11-06 11:44:21 +0000

    Support the case that a call to resolve() returns an empty array because we haven't set Rooftop.include_drafts = true. Also wrap in a rescue block to ensure we always get a nil rather than an explosion

--- lib/rooftop/content/field.rb
+++ lib/rooftop/content/field.rb
@@ -11,19 +11,27 @@ module Rooftop
 
       def resolve
         if respond_to?(:type) && type == "relationship"
-          related_ids = value.collect do |related|
-            if related.is_a?(Hash)
-              related[:ID]
-            else
-              related
+          begin
+            related_ids = value.collect do |related|
+              if related.is_a?(Hash)
+                related[:ID]
+              else
+                related
+              end
             end
-          end
-          klass = Rooftop.configuration.post_type_mapping[relationship[:class].to_sym] || relationship[:class].to_s.classify.constantize
-          resolved = klass.where(id: related_ids).to_a
-          if resolved.length == 1
-            resolved.first
-          else
-            resolved
+            klass = Rooftop.configuration.post_type_mapping[relationship[:class].to_sym] || relationship[:class].to_s.classify.constantize
+            resolved = klass.where(id: related_ids, order_by: :post__in).to_a
+            # there might be an empty array if all the related_ids resolve to posts which are in draft but we don't
+            # have Rooftop.include_drafts set to true. If this is the case, we should return nil
+            if resolved.length == 0
+              nil
+            elsif resolved.length == 1
+              resolved.first
+            else
+              resolved
+            end
+          rescue
+            nil
           end
         else
           value
