5481fc68dccc6a891b1885bbc97d23e436708152
Ed Jones <ed@error.agency>
2017-11-29 15:20:05 +0000

    add fields to MediaItem for source url and caption

--- lib/rooftop/models/media_item.rb
+++ lib/rooftop/models/media_item.rb
@@ -2,5 +2,9 @@ module Rooftop
   class MediaItem
     include Rooftop::Base
     self.api_endpoint =  "media"
+    after_find do |record|
+      record.fields << Rooftop::Content::Field.new({fieldset: "Basic", name: 'source_url', value: record.source_url})
+      record.fields << Rooftop::Content::Field.new({fieldset: "Basic", name: 'caption', value: record.caption[:rendered]})
+    end
   end
 end
